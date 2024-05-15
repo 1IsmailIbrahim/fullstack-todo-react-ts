7531d41dc48df78bf819c4d812a47b159f648f77
Ismail Ibrahim <ismailibrahim6268@gmail.com>
2024-05-15 20:49:28 +0300

    strapi-todo-api

--- src/config/axios.config.ts
+++ src/config/axios.config.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:1337/api",
-  timeout: 1000,
+  baseURL: "https://strapi-todo.onrender.com/api",
 });
 
 export default axiosInstance;
